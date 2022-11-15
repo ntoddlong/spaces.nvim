14a8c94a3ccd44cc0c2610eec2d5b9556c575ab8
Nathaniel Todd-Long <ntoddlong@gmail.com>
2022-11-15 00:07:56 +0000

    Added some stuff, mapping functions doesn't work

--- lua/spaces.lua
+++ lua/spaces.lua
@@ -5,9 +5,21 @@ local R = function(mode, lhs, rhs)
 end
 
 M.setup = function(opts)
+  print("hey")
   for k,v in pairs(opts) do
+    if (type(v) == "table" and k == "mappings") then
+      for mode,maps in pairs(v) do
+        for lhs,rhs in pairs(maps) do
+          print(mode)
+          print(lhs)
+          print(rhs)
+          R(mode, lhs, rhs)
+        end
+      end
+    end
     if (type(v) == "table") then
-      print("-----table------")
+      print("hey")
+      print("key: ",k)
       for k,v in pairs(v) do
         print("key: ",k," value: ",v)
       end
